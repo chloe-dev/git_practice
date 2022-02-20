a7e58f8ed65addaa3910bbbff2a53613960c7195
chloe-dev <chloe.devops@gmail.com>
2022-02-20 15:04:36 -0600

    Add door1 HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,6 @@
+doorImage1 = document.getElementById("door1");
+
+doorImage1.onclick = ()=> {
+  botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
+  doorImage1.src = botDoorPath;
+}
